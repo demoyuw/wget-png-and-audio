f9508172dbf4704aed14162f2474451a49186cb6
YuWei Chou <demoyuw@gmail.com>
2019-07-05 13:12:21 +0800

    fix bug

--- force-get-png.sh
+++ force-get-png.sh
@@ -5,12 +5,12 @@ for i in {1..1000} :
 do
     wget https://www.carry.live/event/card/index.php
     pngname=`cat index.php | grep upload/card/ | grep btn2 | grep action3 | cut -d '"' -f2 | cut -d "/" -f3`
-    if [[ $pngname == *'政客'* ]]; then
-      echo $pngname
-      if [[ ! -f "png/$pngname" ]]; then 
-        wget -P "png"  https://www.carry.live/event/card/upload/card/$pngname
-      fi
+    #if [[ $pngname == *'政客'* ]]; then
+    echo $pngname
+    if [[ ! -f "png/$pngname" ]]; then 
+      wget -P "png"  https://www.carry.live/event/card/upload/card/$pngname
     fi
+    #fi
     rm index.php
     sleep 0.8
 done
